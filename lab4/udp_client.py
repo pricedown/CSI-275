7694324c42e4a3904d33c60559683a7fb7a4f45f
Joseph Isaacs <joseph.isaacs@mymail.champlain.edu>
2025-02-11 17:12:55 -0500

    nitialize lab4

---
+++ lab4/udp_client.py
@@ -0,0 +1,24 @@
+"""Student code for Lab/HW 2.
+
+Champlain College CSI-235, Spring 2019
+The following code was written by Joshua Auerbach (jauerbach@champlain.edu)
+"""
+
+import socket
+import constants
+
+
+def main():
+    """Run some basic tests on the required functionality.
+
+    for more extensive tests run the autograder!
+    """
+    client = UDPClient(constants.HOST, constants.ECHO_PORT)
+    print(client.send_message_by_character("hello world"))
+
+    client = UDPClient(constants.HOST, constants.REQUEST_ID_PORT, True)
+    print(client.send_message_by_character("hello world"))
+
+
+if __name__ == "__main__":
+    main()
