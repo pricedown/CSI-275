8b6dbdde74d7cce46438cd0d560b5305a9c31068
joseph.isaacs <joseph.isaacs@mymail.champlain.edu>
2025-02-12 09:03:57 -0500

    Header info

--- lab4/udp_client.py
+++ lab4/udp_client.py
@@ -1,12 +1,25 @@
-"""Student code for Lab/HW 2.
+"""Practicing with UDP Sockets
+
+Author: Joseph Isaacs
+Class: CSI-275-01
+Assignment: Lab 4 -- UDP Sockets
+
+Certification of Authenticity:
+I certify that this is entirely my own work, except where I have given
+fully-documented references to the work of others. I understand the definition
+and consequences of plagiarism and acknowledge that the assessor of this
+assignment may, for the purpose of assessing this assignment:
+    - Reproduce this assignment and provide a copy to another member of academic
+- staff; and/or Communicate a copy of this assignment to a plagiarism checking
+- service (which may then retain a copy of this assignment on its database for
+- the purpose of future plagiarism checking)
 
 Champlain College CSI-235, Spring 2019
-The following code was written by Joshua Auerbach (jauerbach@champlain.edu)
+The following code was adapted from code written by Joshua Auerbach (jauerbach@champlain.edu)
 """
 
 import socket
 import constants
-
 
 def main():
     """Run some basic tests on the required functionality.
