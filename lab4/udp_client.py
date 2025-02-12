08a33352df9997428b3baddaf340b90b4137bb11
pricedown <josephmisaacs@gmail.com>
2025-02-12 15:31:56 -0500

    Lab4 Q1

--- lab4/udp_client.py
+++ lab4/udp_client.py
@@ -20,6 +20,37 @@ The following code was adapted from code written by Joshua Auerbach (jauerbach@c
 
 import socket
 import constants
+from constants import INITIAL_TIMEOUT, MAX_TIMEOUT
+
+
+class TimeOutError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+class UDPClient:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def send_message_by_character(self, message):
+        self.udp_socket.connect((self.host, self.port))
+        result = ""
+        for character in message:
+            wait_time = INITIAL_TIMEOUT
+            while True:
+                self.udp_socket.settimeout(wait_time)
+                self.udp_socket.sendto(character.encode('ascii'), (self.host, self.port))
+                try:
+                    data, address = self.udp_socket.recvfrom(constants.MAX_BYTES)
+                    result += data.decode('ascii')
+                except TimeoutError:
+                    wait_time *= 2
+                    if wait_time >= MAX_TIMEOUT:
+                        raise TimeOutError("Server unresponsive")
+                else:
+                    break
+        return result
 
 def main():
     """Run some basic tests on the required functionality.
