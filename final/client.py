f9f5c863efa834d25a147f0634716bc283ad10d2
pricedown <joseph.isaacs@mymail.champlain.edu>
2025-04-13 14:54:21 -0400

    Init final

---
+++ final/client.py
@@ -0,0 +1,8 @@
+# A client for a chat program that operates over TCP sockets.
+
+class ChatClient:
+    # def __init__(self, ):
+
+
+if __name__ == '__main__':
+    print("Client started")
---
+++ final/server.py
@@ -0,0 +1,4 @@
+# A threaded server for a chat program that operates over TCP sockets.
+
+if __name__ == '__main__':
+    print("Server started")
