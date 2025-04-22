adf8f51edee29b9ab4553d3327adc609bb381e12
Joseph Isaacs <joseph.isaacs@mymail.champlain.edu>
2025-04-22 07:17:19 -0400

    Lab11

--- lab11/lab11_client.py
+++ lab11/lab11_client.py
@@ -1,4 +1,19 @@
-"""Don't forget your docstrings!
+"""TLS secured server client
+
+Author: Joseph Isaacs
+Class: CSI-275-01
+Assignment: Lab 11 -- Encrypting connections with TLS
+Due Date: Apr 26 2025 at 11:59pm
+
+Certification of Authenticity:
+I certify that this is entirely my own work, except where I have given
+fully-documented references to the work of others. I understand the definition
+and consequences of plagiarism and acknowledge that the assessor of this
+assignment may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of academic
+- staff; and/or Communicate a copy of this assignment to a plagiarism checking
+- service (which may then retain a copy of this assignment on its database for
+- the purpose of future plagiarism checking)
 """
 import socket
 import ssl
@@ -9,19 +24,21 @@ PORT = 7778         # Port of server
 
 def send_messages():
     """Collect text from the user and send it to our server."""
+
     # Create a variable for input
     user_input = ""
 
     context = ssl.create_default_context()
 
+    # load the client's certificate and private key
     context.load_cert_chain(certfile='cli_cert.crt', keyfile="cli_key.key")
+    # load the server's certificate so that it can be verified by the client
     context.load_verify_locations('srv_cert.crt')
-    context.check_hostname = False
-    context.verify_mode = ssl.CERT_REQUIRED
 
     # Create a socket
     raw_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock = context.wrap_socket(raw_sock, server_hostname=HOST)
+    # wrap the socket with our default context
+    sock = context.wrap_socket(raw_sock, server_hostname='localhost')
     sock.connect((HOST, PORT))
 
     while user_input != "done":
@@ -45,4 +62,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
--- lab11/lab11_server.py
+++ lab11/lab11_server.py
@@ -1,4 +1,19 @@
-"""TLS threaded TCP server.
+"""TLS secured threaded TCP server
+
+Author: Joseph Isaacs
+Class: CSI-275-01
+Assignment: Lab 11 -- Encrypting connections with TLS
+Due Date: Apr 26 2025 at 11:59pm
+
+Certification of Authenticity:
+I certify that this is entirely my own work, except where I have given
+fully-documented references to the work of others. I understand the definition
+and consequences of plagiarism and acknowledge that the assessor of this
+assignment may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of academic
+- staff; and/or Communicate a copy of this assignment to a plagiarism checking
+- service (which may then retain a copy of this assignment on its database for
+- the purpose of future plagiarism checking)
 """
 
 import socket
@@ -9,12 +24,12 @@ HOST = 'localhost'  # IP of server
 PORT = 7778         # Port of server
 
 
-def handler(ssl_client_sock, addr):
+def handler(ssl_sock, address):
     """Receive messages and print them to the console."""
     try:
         while True:
             # Receive the data
-            data = ssl_client_sock.recv(4096)
+            data = ssl_sock.recv(4096)
             if not data:
                 break
 
@@ -22,19 +37,20 @@ def handler(ssl_client_sock, addr):
             message = data.decode('ascii')
 
             # Print the message
-            print(message)
+            print(f"Message from {address}: {message}")
     except ssl.SSLError as e:
         print(f"SSL error: {e}")
     finally:
-        ssl_client_sock.close()
+        ssl_sock.close()
 
 if __name__ == "__main__":
-    # Create our listening socket
+    # create a default ssl context
     context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-    context.load_cert_chain(certfile="srv_cert.crt", keyfile="srv_key.key")
 
+    # load the server's certificate and private key
+    context.load_cert_chain(certfile="srv_cert.crt", keyfile="srv_key.key")
+    # load the client's certificate to be verified by the server
     context.load_verify_locations(cafile="cli_cert.crt")
-    context.verify_mode = ssl.CERT_REQUIRED
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -46,6 +62,7 @@ if __name__ == "__main__":
         # handle the accept socket
         client_sock, addr = sock.accept()
         try:
+            # wrap the socket using the ssl context
             ssl_client_sock = context.wrap_socket(client_sock, server_side=True)
             _thread.start_new_thread(handler, (ssl_client_sock, addr))
         except ssl.SSLError as e:
