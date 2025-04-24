14707aa83dda700665d681aec1581b62cbdfb85a
Joseph Isaacs <joseph.isaacs@mymail.champlain.edu>
2025-04-24 11:28:32 -0400

    Final so far

--- final/client.py
+++ final/client.py
@@ -1,29 +1,65 @@
-# A client for a chat program that operates over TCP sockets.
+# A threaded client for a chat program operating over TCP sockets.
 
-def recv_all(self, conn, length):
-    """Receives exactly the amount of data stated in the length field
+import socket
+import threading
+import json
 
-    :param conn: connection to receive from
-    :param length:  length of data to collect
-    :return: the data collected
-    """
+HOST = 'localhost'
+PORT_RECV = 5001
+PORT_SEND = 5000
+
+def recvall(sock, length):
     data = b""
     while len(data) < length:
-        packet = conn.recv(length - len(data))
-        if not packet:
+        more = sock.recv(length - len(data))
+        if not more:
             return data
-        data += packet
+        data += more
     return data
 
+def send_json(sock, data):
+    encoded = json.dumps(data).encode('utf-8')
+    length = len(encoded).to_bytes(4, 'big')
+    sock.sendall(length + encoded)
 
-class ChatClient:
-    def __init__(self, username):
-        self.name = username
+def recv_forever(sock):
+    while True:
+        raw_len = recvall(sock, 4)
+        if not raw_len:
+            break
+        msg_len = int.from_bytes(raw_len, 'big')
+        msg_data = recvall(sock, msg_len)
+        msg = json.loads(msg_data.decode('utf-8'))
 
-    def receiving_thread(self):
-        length_data = recv_all(connection, 4)
-        advertised_length = int.from_bytes(length_data, byteorder='big')
+        # TODO: receive messages
+        if msg[0] == "BROADCAST":
+            print(f"{msg[1]}: {msg[2]}")
 
-if __name__ == '__main__':
-    print("Client started")
+def send_forever(sock, username):
+    while True:
+        msg = input()
+        if msg == "!exit":
+            send_json(sock, ["EXIT", username])
+            break
+        # TODO: send messages
 
+def pick_username():
+    while True:
+        name = input("Enter a username (no spaces): ")
+        if ' ' in name:
+            print("No spaces allowed.")
+        else:
+            return name
+
+if __name__ == "__main__":
+    username = pick_username()
+
+    recv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    recv_sock.connect((HOST, PORT_RECV))
+    send_json(recv_sock, ["START", username])
+
+    send_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    send_sock.connect((HOST, PORT_SEND))
+
+    threading.Thread(target=recv_forever, args=(recv_sock,), daemon=True).start()
+    send_forever(send_sock, username)
--- final/server.py
+++ final/server.py
@@ -1,4 +1,68 @@
-# A threaded server for a chat program that operates over TCP sockets.
+# A threaded server for a chat program operating over TCP sockets.
 
-if __name__ == '__main__':
-    print("Server started")
+import socket
+import threading
+import json
+
+HOST = 'localhost'
+PORT_RECV = 5000
+PORT_SEND = 5001
+
+clients = {} # dictionary of usernames to sockets
+lock = threading.Lock()
+
+def recvall(conn, length):
+    data = b''
+    while len(data) < length:
+        more = conn.recv(length - len(data))
+        if not more:
+            return data
+        data += more
+    return data
+
+def send_json(sock, data):
+    encoded = json.dumps(data).encode('utf-8')
+    length = len(encoded).to_bytes(4, 'big')
+    sock.sendall(length + encoded)
+
+def handle_send_client(conn):
+    while True:
+        pass
+        # TODO: load message w/ length prefix
+
+        # TODO: handle message types
+
+    conn.close()
+
+def handle_recv_client(conn):
+    raw_len = recvall(conn, 4)
+    msg_len = int.from_bytes(raw_len, 'big')
+    msg_data = recvall(conn, msg_len)
+    msg = json.loads(msg_data.decode('utf-8'))
+
+    if msg[0] == "START":
+        username = msg[1]
+        with lock:
+            clients[username] = conn
+            print(f"{username} connected.")
+
+def recv_socket_thread():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((HOST, PORT_RECV))
+    sock.listen()
+    while True:
+        conn, _ = sock.accept()
+        threading.Thread(target=handle_send_client, args=(conn,), daemon=True).start()
+
+def send_socket_thread():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((HOST, PORT_SEND))
+    sock.listen()
+    while True:
+        conn, _ = sock.accept()
+        threading.Thread(target=handle_recv_client, args=(conn,), daemon=True).start()
+
+if __name__ == "__main__":
+    threading.Thread(target=recv_socket_thread, daemon=True).start()
+    send_socket_thread()
